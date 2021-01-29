9abe33a4bcffe4d3cf1558137c61780918c13ea9
RustamIslamnurov <islamnurovrustam@gmail.com>
2021-01-29 18:12:22 +0500

    First master commit

---
+++ src/main/java/com/rustam/programm/ProgrammApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.programm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/rustam/programm/UsersController.java
@@ -0,0 +1,26 @@
+package com.rustam.programm;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class  UsersController {
+
+    @GetMapping()
+    public String getUsers() {
+        return "HTTP GET reqest was sent ";
+    }
+    @PostMapping()
+    public String createUsers() {
+        return "HTTP POST reqest was sent ";
+    }
+    @DeleteMapping()
+    public String deleteUsers() {
+        return "HTTP DELETE reqest was sent ";
+    }
+    @PutMapping()
+    public String putUsers() {
+        return "HTTP PUT reqest was sent ";
+    }
+
+}
